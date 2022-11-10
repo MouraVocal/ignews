8fff0857339ae2b0d561be9f304d99d41470350f
mouravocal <diego.moura3@gmail.com>
2022-11-10 15:27:49 -0300

    feat: github next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,16 +3,27 @@ import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#eba417" />
-      Diego Moura
+      {session.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#04d361" />
       Sign In with Github
     </button>
